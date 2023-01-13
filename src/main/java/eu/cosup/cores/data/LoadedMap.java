f92bd0c6bf1642dd39aae6d44281265ea398d022
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2023-01-13 18:56:25 +0000

    Revert "remove world replacement (cloudnet)"
    This reverts commit 8e1a69a532d257ba0fd8dc89c242f2a4b39a2484.

--- src/main/java/eu/cosup/cores/data/LoadedMap.java
+++ src/main/java/eu/cosup/cores/data/LoadedMap.java
@@ -19,6 +19,8 @@ public class LoadedMap {
     // PLEASE be sure to change this for your need
 
     // object for the loaded map
+
+    private final String name;
     private final ArrayList<Location> teamBlueBeacons;
     private final ArrayList<Location> teamRedBeacons;
     private final Location spectatorSpawn;
@@ -38,11 +40,12 @@ public class LoadedMap {
     private final int zMax;
     private final int zMin;
 
-    public LoadedMap(ArrayList<Location> teamBlueBeacons, ArrayList<Location> teamRedBeacons, Location teamBlueSpawns, Location teamRedSpawns, Location spectatorSpawn, int maxHeight, int minHeight, int deathHeight
+    public LoadedMap(String name, ArrayList<Location> teamBlueBeacons, ArrayList<Location> teamRedBeacons, Location teamBlueSpawns, Location teamRedSpawns, Location spectatorSpawn, int maxHeight, int minHeight, int deathHeight
                     , int xMax, int xMin, int zMax, int zMin) {
 
         // TODO add a rotation aswell so players spawn facing determined location
 
+        this.name = name;
         this.teamBlueSpawns = teamBlueSpawns;
         this.teamRedSpawns = teamRedSpawns;
         this.teamBlueBeacons = teamBlueBeacons;
@@ -55,6 +58,10 @@ public class LoadedMap {
         this.xMin = xMin;
         this.zMax = zMax;
         this.zMin = zMin;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Location getTeamBlueSpawns() {
@@ -165,7 +172,45 @@ public class LoadedMap {
         return getSpawnByTeamColor(teamColor);
     }
 
-    public static LoadedMap loadMapFromConfig() {
+    public void saveToConfig() {
+
+        File configFile = new File(Cores.getInstance().getDataFolder(), "maps.yml");
+
+        if (!configFile.exists()) {
+            try {
+                configFile.createNewFile();
+            } catch (IOException exception) {
+                Bukkit.getLogger().severe("There was no maps file and we were not able to create new one.");
+            }
+        }
+
+        YamlConfiguration customConfig = YamlConfiguration.loadConfiguration(configFile);
+
+        customConfig.set(name + ".maxHeight", maxHeight);
+        customConfig.set(name + ".minHeight", minHeight);
+        customConfig.set(name + ".deathHeight", deathHeight);
+
+        customConfig.set(name + ".teamBlueSpawns", teamBlueSpawns);
+        customConfig.set(name + ".teamRedSpawns", teamRedSpawns);
+
+        customConfig.set(name + ".teamBlueBeacons", teamBlueBeacons);
+        customConfig.set(name + ".teamRedBeacons", teamRedBeacons);
+
+        customConfig.set(name + ".spectatorSpawn", spectatorSpawn);
+
+        customConfig.set(name + ".xMax", xMax);
+        customConfig.set(name + ".zMax", zMax);
+        customConfig.set(name + ".zMin", zMin);
+        customConfig.set(name + ".xMin", xMin);
+
+        try {
+            customConfig.save(configFile);
+        } catch (IOException exception) {
+            Bukkit.getLogger().severe("Were not able to save map to config");
+        }
+    }
+
+    public static LoadedMap loadMapFromConfig(String name) {
 
         File configFile = new File(Cores.getInstance().getDataFolder(), "maps.yml");
 
@@ -176,67 +221,73 @@ public class LoadedMap {
 
         YamlConfiguration customConfig = YamlConfiguration.loadConfiguration(configFile);
 
-        int maxHeight = customConfig.getInt("maxHeight");
-        int minHeight = customConfig.getInt("minHeight");
-        int deathHeight = customConfig.getInt("deathHeight");
-
-        // idk this is not mandatory
-        if (minHeight == maxHeight) {
-            Bukkit.getLogger().severe("   minHeight cannot be the same as the maxHeight plase change that");
+        if (!customConfig.contains(name)) {
+            Bukkit.getLogger().severe("The map that we are trying to load does not exists in maps.yml");
             return null;
         }
 
-        Location teamBlueSpawns = customConfig.getLocation("teamBlueSpawns");
-        Location teamRedSpawns = customConfig.getLocation("teamRedSpawns");
-        Location spectatorSpawn = customConfig.getLocation("spectatorSpawn");
+        int maxHeight = customConfig.getInt(name + ".maxHeight");
+        int minHeight = customConfig.getInt(name + ".minHeight");
+        int deathHeight = customConfig.getInt(name + ".deathHeight");
+
+        // idk this is not mandatory
+        if (minHeight == maxHeight) {
+            Bukkit.getLogger().severe(name + "   minHeight cannot be the same as the maxHeight plase change that");
+            return null;
+        }
+
+        Location teamBlueSpawns = customConfig.getLocation(name + ".teamBlueSpawns");
+        Location teamRedSpawns = customConfig.getLocation(name + ".teamRedSpawns");
+        Location spectatorSpawn = customConfig.getLocation(name + ".spectatorSpawn");
 
         // should probably split this up
         if (teamBlueSpawns == null) {
-            Bukkit.getLogger().severe("blue spawn for " + " was not loaded corectly");
+            Bukkit.getLogger().severe("blue spawn for " + name + " was not loaded corectly");
             return null;
         }
 
         if (teamRedSpawns == null) {
-            Bukkit.getLogger().severe("red spawns for " + " was not loaded corretly");
+            Bukkit.getLogger().severe("red spawns for " + name + " was not loaded corretly");
             return null;
         }
 
         if (spectatorSpawn == null) {
-            Bukkit.getLogger().severe("spectator spawn for " + " was not loaded corectly");
+            Bukkit.getLogger().severe("spectator spawn for " + name + " was not loaded corectly");
             return null;
         }
 
         ArrayList<Location> teamBlueBeacons = new ArrayList<>();
         try {
-            Objects.requireNonNull(customConfig.getList(".teamBlueBeacons"))
+            Objects.requireNonNull(customConfig.getList(name + ".teamBlueBeacons"))
                     .forEach(o -> teamBlueBeacons.add((Location) o));
         } catch (NullPointerException exception) {
             exception.printStackTrace();
-            Bukkit.getLogger().severe("Couldnt load blue team beacons for ");
+            Bukkit.getLogger().severe("Couldnt load blue team beacons for " + name);
             return null;
         }
 
         ArrayList<Location> teamRedBeacons = new ArrayList<>();
         try {
-            Objects.requireNonNull(customConfig.getList("teamRedBeacons"))
+            Objects.requireNonNull(customConfig.getList(name + ".teamRedBeacons"))
                     .forEach(o -> teamRedBeacons.add((Location) o));
         } catch (NullPointerException exception) {
             exception.printStackTrace();
-            Bukkit.getLogger().severe("Couldnt load red team beacons for ");
+            Bukkit.getLogger().severe("Couldnt load red team beacons for " + name);
             return null;
         }
 
-        int xMax = customConfig.getInt("xMax");
-        int zMax = customConfig.getInt("zMax");
-        int zMin = customConfig.getInt("zMin");
-        int xMin = customConfig.getInt("xMin");
+        int xMax = customConfig.getInt(name + ".xMax");
+        int zMax = customConfig.getInt(name + ".zMax");
+        int zMin = customConfig.getInt(name + ".zMin");
+        int xMin = customConfig.getInt(name + ".xMin");
 
         if (xMax == xMin || zMax == zMin) {
-            Bukkit.getLogger().severe("Acording to your configuration for "+" players not by able to place blocks anywhere");
+            Bukkit.getLogger().severe("Acording to your configuration for "+name+" players not by able to place blocks anywhere");
             Bukkit.getLogger().severe("Make sure you have the correct version of maps.yml (check in repository)");
         }
 
         return new LoadedMap(
+                name,
                 teamBlueBeacons,
                 teamRedBeacons,
                 teamBlueSpawns,
@@ -250,5 +301,36 @@ public class LoadedMap {
                 zMax,
                 zMin
         );
+    }
+
+    public static ArrayList<LoadedMap> getLoadedMapsFromConfig() {
+
+        ArrayList<LoadedMap> loadedMaps = new ArrayList<>();
+
+        File configFile = new File(Cores.getInstance().getDataFolder(), "maps.yml");
+
+        if (!configFile.exists()) {
+            Bukkit.getLogger().severe("There is no maps.yml in datafolder while trying to load a map");
+            return null;
+        }
+
+        YamlConfiguration customConfig = YamlConfiguration.loadConfiguration(configFile);
+
+        Set<String> mapNames = customConfig.getKeys(false);
+
+        if (mapNames.size() == 0) {
+            Bukkit.getLogger().severe("There are no maps in maps.yml");
+            return null;
+        }
+
+        for (String name : mapNames) {
+
+            LoadedMap loadedMap = LoadedMap.loadMapFromConfig(name);
+            if (loadedMap != null) {
+                loadedMaps.add(loadedMap);
+            }
+        }
+
+        return loadedMaps;
     }
 }
---
+++ src/main/java/eu/cosup/cores/data/WorldLoader.java
@@ -0,0 +1,75 @@
+package eu.cosup.cores.data;
+
+import eu.cosup.cores.Cores;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class WorldLoader {
+
+
+    // returns false when world failed to load and returns true if it succeeded.
+    public static boolean loadNewWorld(String name) {
+
+        // this is a very brute force way and there is probably a better way of doing this.
+
+        Bukkit.unloadWorld("world", true);
+
+        try {
+            FileUtils.forceDelete(new File("world"));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            Bukkit.getLogger().severe("Couldnt delete directory");
+        }
+
+        try {
+            FileUtils.copyDirectory(new File("plugins/Cores/maps/" + name), new File("world"));
+        } catch (IOException exception) {
+            Bukkit.getLogger().severe("Couldnt copy the folder from maps directory now you should probably worry because this is really bad.");
+            exception.printStackTrace();
+            return false;
+        }
+
+        World world = Bukkit.createWorld(new WorldCreator("world").environment(World.Environment.CUSTOM));
+        Cores.getInstance().setGameWorld(world);
+
+        Bukkit.getLogger().info("Loaded: " + name);
+        return true;
+    }
+
+    public static ArrayList<String> getWorldNames() {
+
+        ArrayList<String> mapNames = new ArrayList<>();
+
+        File mapFolder = new File(Cores.getInstance().getDataFolder(), "maps");
+
+        // make sure the was a folder called maps
+
+        if (mapFolder.mkdir()) {
+            Bukkit.getLogger().warning("Created a folder for maps");
+        }
+
+        File[] mapFiles = mapFolder.listFiles();
+
+        if (mapFiles == null) {
+            Bukkit.getLogger().severe("There are no maps in map folder");
+            return null;
+        }
+
+        for (File mapFile : mapFiles) {
+            if (mapFile.isDirectory()) {
+                if (!mapNames.contains(mapFile.getName())) {
+                    mapNames.add(mapFile.getName());
+                }
+            }
+        }
+
+        return mapNames;
+    }
+
+}
